b8e8d70d1fcafc2107619ada8fcb8c186d5ac5ca
Michaël Dierick <michael.dierick@redpencil.io>
2020-03-25 13:05:53 +0100

    Cache headers

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,4 +1,8 @@
 (in-package :mu-cl-resources)
+(defparameter *cache-model-properties-p* t)
+(defparameter *cache-count-queries* t)
+(defparameter *supply-cache-headers-p* t
+  "when non-nil, cache headers are supplied.  this works together with mu-cache.")
 
 (define-resource email ()
   :class (s-prefix "nmo:Email")
